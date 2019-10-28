8dfa6f7824d7cb3d384400b274e6bc5884ae05d0
vision5011 <409588990@qq.com>
2019-10-28 15:20:20 +0800

    Create 1001s02E04_control_flow.py
    九九乘法表

---
+++ exercises/1901100372/1001s02E04_control_flow.py
@@ -0,0 +1,19 @@
+
+for a in range(1,10):
+    for b in range(1,1+a):
+        print(f'{a} * {b} = {a*b}',end='\t')
+    print()  
+
+
+
+
+
+for i in range(1,10,2):
+    j = 1
+    while j <= i:
+    
+        print(f'{i} * {j} = {i*j}',end='\t')
+        j+=1
+    print()
+
+    
